530a765be69af040c9d70a2e8bb846e47ad58dd2
19520814_phuongnhi <19520814@gm.uit.edu.vn>
2023-07-22 17:38:06 +0700

    update Code new

---
+++ DTO/AccountDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.DTO
+{
+    public class AccountDTO
+    {
+        private string userName;
+        private int iDStaff;
+        private string password;
+        private int iD;
+
+        public string UserName { get => userName; set => userName = value; }
+        public string Password { get => password; set => password = value; }
+        public int ID { get => iD; set => iD = value; }
+        public int IDStaff { get => iDStaff; set => iDStaff = value; }
+
+        public AccountDTO(string userName, int idStaff, int id, string password = null)
+        {
+            this.UserName = userName;
+            this.IDStaff = idStaff;
+            this.Password = password;
+            this.ID = id;
+        }
+
+        public AccountDTO(DataRow row)
+        {
+            this.UserName = row["userName"].ToString();
+            this.IDStaff = (int)row["idStaff"];
+            this.Password = row["passWord"].ToString();
+            this.ID = (int)row["id"];
+        }
+    }
+}
